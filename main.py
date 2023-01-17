71ff6c5b0eceae2416b79d3a16ff61a025f262ff
deschenesr <deschenesr@educ.college-montreal.qc.ca>
2023-01-17 09:44:01 -0500

    nouveaux commit

--- main.py
+++ main.py
@@ -30,19 +30,17 @@ class NPC:
         print('force: ',self.force, '\nagilite: ', self.agilite, '\nconstitution: ', self.constitution,'\nintelligence: ', self.intelligence, '\nsagesse:', self.sagesse, '\ncharisme:', self.charisme, '\nclasse d armure: ', self.classe_armure, '\nnom: ', self.nom, '\nrace: ', self.race, '\nespece: ',self.espece, '\npoint de vie: ',self.point_de_vie, '\nprofession: ', self.profession)
 class kobold(NPC):
     def __init__(self):
-            super().__init__()
+        super().__init__()
     def attaque(self, cible):
-        cible = NPC
-    def subir_dommage(self):
-        self.point_de_vie -= random.randint(1, 6)
+        return
+    def subir_dommage(self, recevoir_dommage):
+        return
+
 
 class hero(NPC):
     def __init__(self):
         super().__init__()
     def attaque(self, cible):
-        cible = NPC
-    def subir_dommage(self):
-        self.point_de_vie -= random.randint(1, 6)
-
-npc1 = NPC()
-npc1.afficher_caracteristique()
+        return
+    def subir_dommage(self, recevoir_dommage):
+        return
